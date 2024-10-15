4b4898dd23792dd413b01f508877a648077822b3
hsan-isetsf <hsan.isetsf@gmail.com>
2024-10-15 10:06:20 +0200

    dist-commit

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const app = express();
 app.use(cors({
     origin:'*'
 }))
-/*app.use(cors({
-    origin:'http://localhost:3000'
-}))*/
+
 //middleware
 app.use(express.json())
+
+//routes
 const categorieRouter=require("./routes/categorie.route")
 const scategorieRouter=require('./routes/scategorie.route')
 const articleRouter=require('./routes/article.route')
@@ -22,12 +22,12 @@ const paymentRouter =require("./routes/payment.route.js");
 const locationRouter =require("./routes/location.route.js");
 
 dotenv.config()
+
+/*
 app.get("/",(req,res)=>{
     res.send("page accueil")
 })
-app.get("/contact",(req,res)=>{
-    res.send("page contact")
-})
+*/
 
 // Connexion à la base données
 mongoose.connect(process.env.DATABASECLOUD)
@@ -36,6 +36,8 @@ mongoose.connect(process.env.DATABASECLOUD)
     console.log('Impossible de se connecter à la base de données', err);
     process.exit();
    });
+
+//appel routes
 app.use("/api/categories",categorieRouter)
 app.use("/api/scategories",scategorieRouter)
 app.use("/api/articles",articleRouter)
@@ -43,9 +45,11 @@ app.use('/api/users', userRouter);
 app.use('/api/payment', paymentRouter);
 app.use('/api/locations', locationRouter);
 
+//dist reactjs
 app.use(express.static(path.join(__dirname, './client/build'))); // Route pour les pages non trouvées, redirige vers index.html 
 app.get('*', (req, res) => { res.sendFile(path.join(__dirname, './client/build/index.html')); });
 
+//serveur
 app.listen(process.env.PORT)
 console.log("application executée sur le port " + process.env.PORT)
 module.exports = app;
